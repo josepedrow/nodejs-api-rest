087c71fe7347116a3796850fa269444be3c3c7f1
JosePw <josepw@hotmail.com>
2022-03-16 09:39:48 -0300

    Adicionando nodemon para auto update e inicialização de index.js

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ app.listen(3000, () => console.log("servidor rodando na porta 3000"));
 //rodar o servidor no terminal
 
 //req solicitado, res responda
-app.get("/atendimentos", (req, res) => res.send("Você está na rota de atendimentos")); // /atendimentos será uma página
+//usando o nodemon as atualizações do servidor são automáticas agora
+app.get("/atendimentos", (req, res) => res.send("Você está na rota de atendimentos e está realizando um get")); // /atendimentos será uma página
