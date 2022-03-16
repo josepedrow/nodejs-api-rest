ce2edd7be0051f2da0620b9bbf194ea3c61eeebe
JosePw <josepw@hotmail.com>
2022-03-16 10:24:12 -0300

    Adiciondo bodyParser para usar no express e criando app.post

--- config/custon-express.js
+++ config/custon-express.js
@@ -1,13 +1,19 @@
 //CONFIGURAÇÕES
 
 //chamar modulos, importá-los
-const express = require("express");
-const consign = require("consign");
+const express = require("express")
+const consign = require("consign")
+const bodyParser = require("body-parser")
 
 module.exports = () => {
 
     //criar um aplicativo
-    const app = express();
+    const app = express()
+
+    //add bibliotecas par usar no express
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+
 
     //modulos de controles para serem acessados pelo app
     consign()
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -7,6 +7,13 @@ module.exports = app => {
     //GERENCIAR ROTA DE ATENDIMENTO
     //req solicitado, res responda
     //usando o nodemon as atualizações do servidor são automáticas agora
-    app.get("/atendimentos", (req, res) => res.send("Você está na rota de atendimentos e está realizando um get")); // /atendimentos será uma página
+    app.get("/atendimentos", (req, res) => res.send("Você está na rota de atendimentos e está realizando um GET")); // /atendimentos será uma página
+
+    //cliente enviando algo
+    app.post("/atendimentos", (req, res) => {
+        console.log(req.body)
+        res.send("Você está na rota de atendimento e está realizando um POST")
+
+    })
 
 }
